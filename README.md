e1cc83f8026552846af83609c202a81b42956bd8
Andrei Poenaru <andrei.poenaru@gmail.com>
2017-08-21 16:57:59 +0100

    Read settings and comic watchlist from config files.

--- README.md
+++ README.md
@@ -16,6 +16,15 @@ pip3 install --upgrade feedparser beauutifulsoup4
 
 **TODO**: A future version may allow installation through pip.
 
+## Configuration
+
+Before you can use Comics Mailer, you need to configure it. This is done thorugh two main files:
+
+* `$HOME/.config/comics-mailer/params.cfg` is an ini-like configuration file that contains the Mailgun API key and domain used to send emails. See  `params.cfg.template` in this repo for the paramters you need to configure.
+* `$HOME/.config/comics-mailer/watchlist.lst` is the list of comics for which you want to receive alerts. Enter a (partial) title on each line; blank lines and lines starting with `#` are ignored, and the matching is case-insensitive.
+
+**TODO**: A future version may include an interactive initial setup procedure.
+
 ## Usage
 
 The suggested way to run Comics Mailer is to use schedule a weekly cron job for it. See `man crontab` if you haven't used cron before.
@@ -27,7 +36,7 @@ To run the script every Wednesday at 6 pm, you would use the following job:
   0 18 *   *    3    /path/to/comics_mailer.py
 ```
 
-**TODO**: Before you can send emails, you need to configure your Mailgun parameters.
+**Important**: Make sure you have set up your installation as described in [the Configuration section](#configuration). The script will _not_ work unless all the settings are in place.
 
 ## Credits
 
--- comics_mailer.py
+++ comics_mailer.py
@@ -2,16 +2,68 @@
 
 import re
 import sys
+import os.path
+from pathlib import Path
+from configparser import ConfigParser
 
 import feedparser
 from bs4 import BeautifulSoup as soup
+
+CONFIG_FOLDER         = os.path.join(Path.home(), '.config/comics-mailer')
+CONFIG_FILE_PARAMS    = os.path.join(CONFIG_FOLDER, 'params.cfg')
+CONFIG_FILE_WATCHLIST = os.path.join(CONFIG_FOLDER, 'watchlist.lst')
+
+CONFIG_SECTION_MAILGUN    = 'mailgun'
+CONFIG_KEY_MAILGUN_KEY    = 'api_key'
+CONFIG_KEY_MAILGUN_DOMAIN = 'domain'
 
 FEED_URL = 'http://feeds.feedburner.com/ncrl'
 
 # TODO: remove debugging
 DEBUG = True
 
-ERR_NO_FEED = -1
+ERR_NO_FEED        = -1
+ERR_INVALID_PARAMS = -2
+ERR_NO_CONFIG      = -3
+ERR_NO_WATCHLIST   = -4
+
+# Print an error message and exit
+def err_exit(msg, code=1):
+    print(msg, file=sys.stderr)
+    sys.exit(code)
+
+# Read the required mailgun parameters from the config file
+def read_mailgun_params():
+    if not os.path.isfile(CONFIG_FILE_PARAMS):
+        err_exit("No configuration file found at at " + CONFIG_FILE_PARAMS + \
+            ". Please see the README for details on creating one.", ERR_NO_CONFIG)
+
+    config = ConfigParser()
+    config.read(CONFIG_FILE_PARAMS)
+
+    for s in [CONFIG_SECTION_MAILGUN]:
+        if s not in config:
+            err_exit("Invailid Mailgun configuration. Please check the config file at " + \
+                CONFIG_FILE_PARAMS + " and see the README for details.", ERR_INVALID_PARAMS)
+
+    for k in [CONFIG_KEY_MAILGUN_KEY, CONFIG_KEY_MAILGUN_DOMAIN]:
+        if k not in config[CONFIG_SECTION_MAILGUN]:
+            err_exit("Invailid Mailgun configuration. Please check the config file at " + \
+                CONFIG_FILE_PARAMS + " and see the README for details.", ERR_INVALID_PARAMS)
+
+    return config[CONFIG_SECTION_MAILGUN][CONFIG_KEY_MAILGUN_KEY], \
+        config[CONFIG_SECTION_MAILGUN][CONFIG_KEY_MAILGUN_DOMAIN]
+
+# Read the watched comics list
+def read_watchlist():
+    if not os.path.isfile(CONFIG_FILE_WATCHLIST):
+        err_exit("You have no watched comics. Please place your comics in " + \
+            CONFIG_FILE_WATCHLIST + ". See the README for details.", ERR_NO_WATCHLIST)
+
+    with open(CONFIG_FILE_WATCHLIST, 'r') as f:
+        watchlist = [line.strip() for line in f if line.strip() != '' and not line.strip().startswith('#')]
+
+    return watchlist
 
 # Deprecated
 def make_feed_url(base, query):
@@ -36,10 +88,9 @@ def get_entry_details(entry):
 def get_rss_entries():
     feed = feedparser.parse(FEED_URL)
     if len(feed.entries) == 0 or 'bozo_exception' in feed:
-        print("Could not retrieve feed contents. Is your URL working?")
+        err_exit("Could not retrieve feed contents. Is your URL working?", ERR_NO_FEED)
         if not DEBUG:
             send_mail_error(ERR_NO_FEED)
-        sys.exit(ERR_NO_FEED)
 
     return feed.entries
 
@@ -48,7 +99,18 @@ def parse_comic_list(entry):
     return list(soup(entry.summary, "lxml").find_all('p')[4].stripped_strings)
 
 if __name__ == '__main__':
-    # TODO: Read the Mailgun params from a config file and/or CLI arguments.
+    # TODO: Consider adding CLI arguments for the mailgun params
+    # TODO: add a --setup option that walks thourgh typing in the settings and setting the watch list
+
+    mailgun_key, mailgun_domain = read_mailgun_params()
+    if not mailgun_key or not mailgun_domain:
+        err_exit("Invailid Mailgun configuration. Please check the config file at " + \
+                CONFIG_FILE_PARAMS + " and see the README for details.", ERR_INVALID_PARAMS)
+
+    watchlist = read_watchlist()
+    if not watchlist:
+        err_exit("You have no watched comics. Please place your comics in " + \
+            CONFIG_FILE_WATCHLIST + ". See the README for details.", ERR_NO_WATCHLIST)
 
     # Use only the latest week
     # TODO: Use all entries since the last update
